3b83cf660268d3ea1369ea8532a7221bcaaa2ffd
cqlyj <luoyingjie0721@gmail.com>
2024-10-13 23:24:02 +0800

    first pass manual review for IFlashLoanReceiver

--- src/interfaces/IFlashLoanReceiver.sol
+++ src/interfaces/IFlashLoanReceiver.sol
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity 0.8.20;
 
-import { IThunderLoan } from "./IThunderLoan.sol";
+// @audit-info it's bad practice to edit live code for test/mocks, we need to remove the import from `MockFlashLoanReceiver.sol`
+import {IThunderLoan} from "./IThunderLoan.sol";
 
 /**
  * @dev Inspired by Aave:
  * https://github.com/aave/aave-v3-core/blob/master/contracts/flashloan/interfaces/IFlashLoanReceiver.sol
  */
 interface IFlashLoanReceiver {
+    // @audit-info where is the natspec?
     function executeOperation(
         address token,
         uint256 amount,
         uint256 fee,
         address initiator,
         bytes calldata params
-    )
-        external
-        returns (bool);
+    ) external returns (bool);
 }
