7d299722d5cf6cfd74d4aa8424c60f36ba00ba15
cqlyj <luoyingjie0721@gmail.com>
2024-10-13 23:23:40 +0800

    first pass manual review for IPoolFactory

--- src/interfaces/IPoolFactory.sol
+++ src/interfaces/IPoolFactory.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.20;
 
+// q why are we using T-Swap?
 interface IPoolFactory {
     function getPool(address tokenAddress) external view returns (address);
 }
