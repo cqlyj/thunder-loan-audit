f2501b178e4fe56173dfcaeb7af747265140d0ad
cqlyj <luoyingjie0721@gmail.com>
2024-10-13 23:24:22 +0800

    first pass manual review for ITSwapPool

--- src/interfaces/ITSwapPool.sol
+++ src/interfaces/ITSwapPool.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.20;
 
+// q why are we only using the price of a pool token in WETH?
 interface ITSwapPool {
     function getPriceOfOnePoolTokenInWeth() external view returns (uint256);
 }
