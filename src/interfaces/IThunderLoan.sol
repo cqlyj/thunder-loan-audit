5be49d2f7d994952b6f8b18d431c9d12ba288e27
cqlyj <luoyingjie0721@gmail.com>
2024-10-13 23:24:12 +0800

    first pass manual review for IThunderLoan

--- src/interfaces/IThunderLoan.sol
+++ src/interfaces/IThunderLoan.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
+// @audit-info the IThunderLoan interface should be implemented by the ThunderLoan contract
 interface IThunderLoan {
+    // @audit-low/info ???
     function repay(address token, uint256 amount) external;
 }
